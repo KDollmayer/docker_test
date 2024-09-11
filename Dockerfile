5b8a12543e5f8e730acc88b967520893d681cbd8
Karl Dollmayer <karl.dollmayer@daresay.co>
2024-09-11 09:50:45 +0200

    add docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:20-alpine
+
+RUN npm install -g pnpm
+
+WORKDIR /usr/src/app
+
+COPY package.json pnpm-lock.yaml ./
+
+RUN pnpm install --frozen-lockfile
+
+COPY . .
+
+EXPOSE 8080
+
+CMD [ "pnpm", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
+  debugger;
+  res.json({ message: 'Hello blabla' });
 });
 const PORT = Number(process.env.PORT || 8080);
 app.listen(PORT, '0.0.0.0', () => {
