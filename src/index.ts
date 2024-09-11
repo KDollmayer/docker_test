3b30d759ca13569cc773b8918f79b0b72705d858
Karl Dollmayer <karl.dollmayer@daresay.co>
2024-09-11 09:13:09 +0200

    added express, prettier, eslint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import express from 'express';
+import morgan from 'morgan';
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+const PORT = Number(process.env.PORT || 8080);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
